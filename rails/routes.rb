da321504cbd66487f232cd40e5af4f83d9ea1f95
Marcio Trindade <marciotrindade@gmail.com>
2010-09-06 02:15:57 -0300

    remove rails3 routes

--- rails/routes.rb
+++ rails/routes.rb
@@ -1,7 +1,7 @@
-if defined?(Rails::Application)
-  # Rails3 routes
-  Rails.application.routes.draw do
-    match "/#{Jammit.package_path}/:package.:extension",
-      :to => 'jammit#package', :as => :jammit
-  end
-end
+# if defined?(Rails::Application)
+#   # Rails3 routes
+#   Rails.application.routes.draw do
+#     match "/#{Jammit.package_path}/:package.:extension",
+#       :to => 'jammit#package', :as => :jammit
+#   end
+# end
